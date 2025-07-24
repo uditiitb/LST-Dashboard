5d41a998bae4a96516c19f7828fd9d5688d1749e
Udit Sarkar <159702011+uditiitb@users.noreply.github.com>
2025-07-24 19:40:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from datetime import datetime, timedelta
 from datetime import date
 from dateutil.relativedelta import relativedelta          # pip install python-dateutil
 import calendar
-import cdsapi
 import xarray as xr
 import pandas as pd
 
