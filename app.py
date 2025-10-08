5ed61f2efbdfa213a93e15a437c23221e3e36505
Udit Sarkar <159702011+uditiitb@users.noreply.github.com>
2025-10-08 12:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -763,5 +763,8 @@ def calendar_plot(city, year, month):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    import os
+    port = int(os.environ.get("PORT", 8080))  # use Render’s assigned port
+    app.run(host="0.0.0.0", port=port)
+
 
